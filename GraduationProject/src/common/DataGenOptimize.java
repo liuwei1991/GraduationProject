cb3c2d2344b0f187ac93e521972bb2733b8ca964
PC <PC@PC-PC>
2015-04-01 19:58:00 +0800

    l

---
+++ GraduationProject/src/common/DataGenOptimize.java
@@ -0,0 +1,105 @@
+package common;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class DataGenOptimize {
+
+	int num = 0;
+	int keyLen = 8;
+	int columnNum = 4;
+	String filePath = "";
+	
+	public DataGenOptimize(int num,int keyLen,int columnNum,String filePath){
+		this.num = num;
+		this.keyLen = keyLen;
+		this.filePath = filePath;
+		this.columnNum = columnNum;
+	}
+	
+	public void genData() throws IOException{
+		File file = new File(this.filePath+".txt");
+		
+		if(file.exists()){
+			System.out.println("File exists, newly created.");
+		}
+		file.createNewFile();
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		Random r = new Random();
+		int totalNum = 0;
+		
+		for(int i=0;i<this.num;i++){
+			String line = "";
+			for(int j=0;j<keyLen;j++){
+				line += String.valueOf(r.nextInt(10));
+			}
+			for(int j=0;j<this.columnNum;j++){
+				line+=" "+r.nextInt(10000);
+			}
+			bw.write(line+"\r\n");
+			totalNum++;
+		}
+		bw.flush();
+		bw.close();
+		fw.close();
+		System.out.println("Number: "+totalNum);
+	}
+	
+	public void genData(int fileNum) throws IOException{
+		int numPerFile = this.num/fileNum;
+		int totalNum = 0;
+		File file = new File(this.filePath);
+		file.mkdirs();
+		for(int curNum=0;curNum<fileNum;curNum++){
+			file = new File(this.filePath+"/"+curNum+".txt");
+			
+			if(file.exists()){
+				System.out.println("File exists, newly created.");
+			}
+			file.createNewFile();
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			Random r = new Random();
+			
+			for(int i=0;i<numPerFile;i++){
+				String rowKey = "";
+				for(int j=0;j<keyLen;j++){
+					rowKey += String.valueOf(r.nextInt(10));
+				}
+				for(int j=0;j<this.columnNum;j++){
+					rowKey+=" "+r.nextInt(10000);
+				}
+				bw.write(rowKey+"\r\n");
+				totalNum++;
+			}
+			bw.flush();
+			bw.close();
+			fw.close();
+			System.out.println("File-"+curNum+" number: "+numPerFile);
+		}
+		System.out.println("Total number: "+totalNum);
+	}
+	
+	public static void main(String[] args) throws IOException{
+		int base = 10000;
+		int keylen[] = {24,32};
+		int threadNum = 10;
+		int columnNum = 4;
+		
+		for(int len:keylen){
+			for(int i=1;i<=10;i++){
+				DataGenOptimize dg = new DataGenOptimize(500*i*base,len,columnNum,"D:/TestData/t2/keylen="+len+" columnNum="+columnNum+"/"+500*i+"w");
+				if(threadNum<=1){
+					dg.genData();
+				}else{
+					dg.genData(threadNum);
+				}
+			}
+		}
+	}
+
+}
